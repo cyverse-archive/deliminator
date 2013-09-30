c83d3b20c7ed2cfd09210fc180e66829bb093b68
Dennis Roberts <dennis@iplantcollaborative.org>
2013-09-30 11:28:37 -0700

    changed the group ID for the artifact

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deliminator "0.1.0-SNAPSHOT"
+(defproject org.iplantc/deliminator "0.1.0-SNAPSHOT"
   :description "Clojure library for parsing character separated values."
   :url "https://github.com/iPlantCollaborativeOpenSource/deliminator"
   :license {:name "BSD Standard License"
