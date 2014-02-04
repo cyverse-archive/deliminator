72d722fd4bc48170246bfb217d902c2b3c1978e8
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-04 12:58:35 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,15 @@
   :url "https://github.com/iPlantCollaborativeOpenSource/deliminator"
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/kameleon.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [slingshot "0.10.3"]])
+                 [slingshot "0.10.3"]]
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
